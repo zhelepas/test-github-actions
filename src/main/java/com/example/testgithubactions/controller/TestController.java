06c4586cc4e163cc4c42f4614f04950f8430698c
p-zhelev-diconium <pasko.zhelev@diconium.com>
2023-02-12 09:28:20 +0100

    modifications

--- src/main/java/com/example/testgithubactions/controller/TestController.java
+++ src/main/java/com/example/testgithubactions/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping
-    public ResponseEntit<String> test() {
+    public ResponseEntity<String> test() {
 
         final var testVar = "Test Var";
 
