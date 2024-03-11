0d8216426dbfc214e0c044e21bc54b72fb4aa91f
zhelepas <pasko.zhelev@diconium.com>
2024-03-11 14:35:24 +0100

    test

--- src/main/java/com/example/testgithubactions/controller/TestController.java
+++ src/main/java/com/example/testgithubactions/controller/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
     @GetMapping
     public ResponseEntity<String> test() {
 
-        final var testVar = "Test Var";
+        final var testVar = "Test Var1";
 
         return ResponseEntity.ok("Ok");
     }
